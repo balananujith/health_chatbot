2b20c66e26983109be571f54a3b71fe3f40620d4
Anujith Balan <balananujith@gmail.com>
2024-04-15 11:12:08 +0530

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// JavaScript code for handling card clicks
-document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll(".card");
+let menubar = document.querySelector('#menubar');
+let mynav = document.querySelector('.navbar');
 
-    cards.forEach(function(card) {
-        card.addEventListener("click", function() {
-            const cardTitle = card.querySelector(".card-title").textContent;
-            console.log("Clicked on", cardTitle);
-            // You can add more functionality here, such as displaying details of the service
-        });
-    });
-});
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times')
+    mynav.classList.toggle('active')
+}
+
